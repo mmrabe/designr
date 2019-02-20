71129c6101e12c18efe4a4b445909a84b082575e
Max Rabe <rabe.maximilian@gmail.com>
2019-02-20 14:08:20 +0100

    Move files to top directory

--- build.R
+++ build.R
@@ -1,7 +1,7 @@
 
 detach("package:designr")
-devtools::document("package")
-devtools::build("package", manual=T)
+devtools::document(".")
+devtools::build(".", manual=T)
 remove.packages("designr")
 archives <- list.files(pattern="^designr_.*\\.tar\\.gz$")
 install.packages(archives[length(archives)], repos=NULL)
