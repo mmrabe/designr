852c1b1b98bf6c35beef35294f2b6c3deedc5cd3
Max Rabe <rabe.maximilian@gmail.com>
2019-09-28 11:10:43 +0200

    remove non-standard files

--- build.R
+++
@@ -1,7 +0,0 @@
-
-detach("package:designr")
-devtools::document(".")
-devtools::build(".", manual=T)
-remove.packages("designr")
-archives <- list.files("..", pattern="^designr_.*\\.tar\\.gz$", full.names = T)
-install.packages(archives[length(archives)], repos=NULL)
