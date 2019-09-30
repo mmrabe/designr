2debaac35485b84f6316d3358aed952165e66893
Max Rabe <rabe.maximilian@gmail.com>
2019-09-30 09:13:54 +0200

    Faster table joining using tibble joins

--- R/toolbox.R
+++ R/toolbox.R
@@ -46,41 +46,76 @@ and <- function(...) {
   ret
 }
 
-join <- function(...) {
-  elements <- list(...)
-  if(!all(vapply(elements, is.data.frame, logical(1)))) stop("All arguments must be data.frames!")
-  if(length(elements) == 0L) return(tibble::tibble())
-  ret <- elements[[1]]
-  rownames(ret) <- NULL
-  for(element in elements[-1]) {
-    if(nrow(element)==0) next
-    rownames(element) <- NULL
-    if(nrow(ret)==0) {
-      ret <- element
-      next
-    }
-    col.matches <- intersect(colnames(element), colnames(ret))
-    new.col.ids <- setdiff(colnames(element), colnames(ret))
-    old.col.ids <- setdiff(colnames(ret), colnames(element))
-    matches.x <- integer(0)
-    matches.y <- integer(0)
-    for(row in seq_len(nrow(ret))) {
-      if(length(col.matches)==0L) new.rows <- seq_len(nrow(element))
-      else {
-        new.rows <- seq_len(nrow(element))
-        for(col in col.matches) {
-          if(is.na(ret[row, col])) next
-          new.rows <- new.rows[is.na(element[new.rows,col])|element[new.rows,col]==ret[row, col]]
-        }
+na_join <- function(.data, b) {
+  b <- dplyr::as_tibble(b)
+  .data <- dplyr::as_tibble(.data)
+  colmatches <- intersect(colnames(.data), colnames(b))
+  if(length(colmatches) == 0) {
+    cbind(.data[rep(seq_len(nrow(.data)), each=nrow(b)),,drop=FALSE], b)
+  } else {
+    where.na <- is.na(b[,colmatches,drop=FALSE])
+    d2 <- do.call(dplyr::group_by, c(list(.data = as.data.frame(where.na)), rlang::syms(colmatches)))
+    indices <- dplyr::group_rows(d2)
+    b.uniq.cols <- setdiff(colnames(b), colmatches)
+    for(ix in indices) {
+      what.na <- where.na[ix[1],]
+      if(!any(what.na)) {
+        next
       }
-      matches.x <- c(matches.x, rep(row, length(new.rows)))
-      matches.y <- c(matches.y, new.rows)
+      na.cols <- colmatches[what.na]
+      vals.in.a <- unique(.data[,na.cols,drop=FALSE])
+      b <- rbind(b[-ix,,drop=FALSE], cbind(b[rep(ix, each=nrow(vals.in.a)), b.uniq.cols, drop=FALSE], vals.in.a))
     }
-    ret <- cbind(ret[matches.x, , drop=F], element[matches.y, new.col.ids, drop=F])
-    rownames(ret) <- NULL
+    dplyr::inner_join(.data, b, by = colmatches)
   }
-  tibble::as.tibble(ret)
 }
+
+join <- function(...) {
+  dfs <- list(...)
+  if(!all(vapply(dfs, is.data.frame, FALSE))) stop("All arguments to `join` must be data frames!")
+  if(length(dfs) == 0) return(tibble())
+  ret <- dfs[[1]]
+  for(el in dfs[-1]) {
+    ret <- na_join(ret, el)
+  }
+  ret
+}
+
+#join_bk <- function(...) {
+#  elements <- list(...)
+#  if(!all(vapply(elements, is.data.frame, logical(1)))) stop("All arguments must be data.frames!")
+#  if(length(elements) == 0L) return(tibble::tibble())
+#  ret <- elements[[1]]
+#  rownames(ret) <- NULL
+#  for(element in elements[-1]) {
+#    if(nrow(element)==0) next
+#    rownames(element) <- NULL
+#    if(nrow(ret)==0) {
+#      ret <- element
+#      next
+#    }
+#    col.matches <- intersect(colnames(element), colnames(ret))
+#    new.col.ids <- setdiff(colnames(element), colnames(ret))
+#    old.col.ids <- setdiff(colnames(ret), colnames(element))
+#    matches.x <- integer(0)
+#    matches.y <- integer(0)
+#    for(row in seq_len(nrow(ret))) {
+#      if(length(col.matches)==0L) new.rows <- seq_len(nrow(element))
+#      else {
+#        new.rows <- seq_len(nrow(element))
+#        for(col in col.matches) {
+#          if(is.na(ret[row, col])) next
+#          new.rows <- new.rows[is.na(element[new.rows,col])|element[new.rows,col]==ret[row, col]]
+#        }
+#      }
+#      matches.x <- c(matches.x, rep(row, length(new.rows)))
+#      matches.y <- c(matches.y, new.rows)
+#    }
+#    ret <- cbind(ret[matches.x, , drop=F], element[matches.y, new.col.ids, drop=F])
+#    rownames(ret) <- NULL
+#  }
+#  tibble::as.tibble(ret)
+#}
 
 sorto <- function(vec, order) {
   vec[order(match(vec, order))]
